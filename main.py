3adb973d21c5d0e814384f565270030f6f8fd91d
VONING <41940033+VONING@users.noreply.github.com>
2020-09-03 14:13:44 +0800

    MakeCode 项目的初始文件

---
+++ main.py
@@ -0,0 +1,24 @@
+tree: Sprite = None
+step = 20
+for indexx in range(5):
+    for indexy in range(3):
+        tree = sprites.create(img("""
+                . . . . . . . e c 7 . . . . . . 
+                            . . . . e e e c 7 7 e e . . . . 
+                            . . c e e e e c 7 e 2 2 e e . . 
+                            . c e e e e e c 6 e e 2 2 2 e . 
+                            . c e e e 2 e c c 2 4 5 4 2 e . 
+                            c e e e 2 2 2 2 2 2 4 5 5 2 2 e 
+                            c e e 2 2 2 2 2 2 2 2 4 4 2 2 e 
+                            c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+                            c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+                            c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+                            c e e 2 2 2 2 2 2 2 2 2 2 4 2 e 
+                            . e e e 2 2 2 2 2 2 2 2 2 4 e . 
+                            . 2 e e 2 2 2 2 2 2 2 2 4 2 e . 
+                            . . 2 e e 2 2 2 2 2 4 4 2 e . . 
+                            . . . 2 2 e e 4 4 4 2 e e . . . 
+                            . . . . . 2 2 e e e e . . . . .
+            """),
+            SpriteKind.player)
+        tree.set_position(40 + indexx * step, 10 + indexy * step)
